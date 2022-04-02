3db9510a76e66d8574bedbaa6c222091c5650962
aokihu <aokihu@gmail.com>
2022-04-02 22:57:21 +0800

    add node package and ignore build folder

---
+++ types/types.d.ts
@@ -0,0 +1,45 @@
+export declare const TYPE_TO_CODE: {
+    u8: number;
+    i8: number;
+    u16: number;
+    i16: number;
+    u32: number;
+    i32: number;
+    u64: number;
+    i64: number;
+    float: number;
+    double: number;
+    char: number;
+    uchar: number;
+    string: number;
+};
+export declare const CODE_TO_TYPE: {
+    10: string;
+    11: string;
+    12: string;
+    13: string;
+    14: string;
+    15: string;
+    16: string;
+    17: string;
+    18: string;
+    19: string;
+    20: string;
+    21: string;
+    22: string;
+};
+export declare const CODE_TO_VIEWTYPE: {
+    10: Uint8ArrayConstructor;
+    11: Int8ArrayConstructor;
+    12: Uint16ArrayConstructor;
+    13: Int16ArrayConstructor;
+    14: Uint32ArrayConstructor;
+    15: Int32ArrayConstructor;
+    16: BigUint64ArrayConstructor;
+    17: BigInt64ArrayConstructor;
+    18: Float32ArrayConstructor;
+    19: Float64ArrayConstructor;
+    20: Uint8ArrayConstructor;
+    21: Uint8ArrayConstructor;
+    22: Uint8ArrayConstructor;
+};
