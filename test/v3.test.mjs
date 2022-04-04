37249ce949c870d76284b0e8d9fcf69240698e07
aokihu <aokihu@gmail.com>
2022-04-04 11:17:29 +0800

    add test cases

--- test/v3.test.mjs
+++ test/v3.test.mjs
@@ -1,19 +1,101 @@
+import assert from 'assert'
 import { JSCStruct } from '../build/index.js'
 
 describe("JSCStruct class test", () => {
 
-    it("Anonymous struct test", () => {
-        const str = `
-            struct {
-                u8 first;
-                u8 second[2];
-                string word[5];
-            }
-        `
+    describe("Anonymous struct test", () => {
 
-        const buf = new Uint8Array([16, 12, 32, 0x68, 0x65, 0x6c, 0x6c, 0x6f])
-        const struct = new JSCStruct(str);
-        const obj = struct.decode(buf).toJson();
+        it("uint8 element and equal 16", () => {
+            const str = `
+                struct {
+                    u8 a;
+                }
+            `
+            const buf = new Uint8Array([16])
+            const struct = new JSCStruct(str);
+            const obj = struct.decode(buf).toJson();
+            assert.strictEqual(obj.a, 16, 'first element is not equal 16')
+        })
+    
+        it("int8 element and equal -16", () => {
+            const str = `
+                struct {
+                    i8 a;
+                }
+            `
+            const buf = new Int8Array([-16])
+            const struct = new JSCStruct(str);
+            const obj = struct.decode(buf).toJson();
+            assert.strictEqual(obj.a, -16, 'first element is not equal -16')
+        })
+
+        it("uint8 element and not equal -16", () => {
+            const str = `
+                struct {
+                    u8 a;
+                }
+            `
+            const buf = new Int8Array([-16])
+            const struct = new JSCStruct(str);
+            const obj = struct.decode(buf).toJson();
+            assert.notEqual(obj.a, -16, 'first element is not equal -16')
+        })
+
+        it("uint16 element and equal 3124", () => {
+            const str = `
+                struct {
+                    u16 a;
+                }
+            `
+            const buf = new Uint16Array([3124])
+            const struct = new JSCStruct(str);
+            const obj = struct.decode(buf).toJson();
+            assert.strictEqual(obj.a, 3124, 'first element is not equal 3124')
+        })
+
+        it("uint32 element and equal 593124", () => {
+            const str = `
+                struct {
+                    u32 a;
+                }
+            `
+            const buf = new Uint32Array([593124])
+            const struct = new JSCStruct(str);
+            const obj = struct.decode(buf).toJson();
+            assert.strictEqual(obj.a, 593124, 'first element is not equal 593124')
+        })
+
+        it("element is u8 array", () => {
+            const str = `
+                struct {
+                    u8 a[3];
+                }
+            `
+
+            const buf = new Uint8Array([15, 16, 17])
+            const struct = new JSCStruct(str)
+            const obj = struct.decode(buf).toJson()
+            assert.deepEqual(obj.a, [15, 16, 17], 'element is not a uin8 array')
+        })
+
+        it("element is chars", () => {
+
+            const str = `
+                struct {
+                    char a[5]
+                }
+            `
+
+            const buf = Uint8Array.from("hello".split('').map(w => w.charCodeAt(0)))
+            const struct = new JSCStruct(str)
+            const obj = struct.decode(buf).toJson();
+            assert.deepEqual(obj.a, 'hello', 'string is not "hello"')
+
+        })
+
+        
     })
+
+    
 
 })
