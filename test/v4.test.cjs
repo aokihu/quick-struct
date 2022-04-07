a2cfed44acab4300dbf3faafcce15dcb3c1142df
aokihu <aokihu@gmail.com>
2022-04-07 11:03:39 +0800

    use esbuild instead webpack

---
+++ test/v4.test.cjs
@@ -0,0 +1,14 @@
+const {qs} = require('../build/index.js')
+
+describe("Encode testcases", () => {
+
+    it("", () => {
+        const struct = qs`
+            struct {
+                u8 a;
+            }
+        `
+    })
+
+
+})
