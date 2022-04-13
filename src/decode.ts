8be2e47d9f8beab674f5045477583dce7642109d
aokihu <aokihu@gmail.com>
2022-04-13 23:14:26 +0800

    split decode code into isolate file

--- src/decode.ts
+++ src/decode.ts
@@ -1,1 +1,67 @@
-export function decode() {}
+import { QStruct } from ".";
+import { CODE_TO_BYTE_SIZE, CODE_TO_DV_TYPE } from "./types_map";
+
+/**
+ * Decode binary to javascript data
+ * @param buffer binary data
+ * @param structName struct name
+ * @returns class instance self
+ */
+export function decode(this: QStruct, buffer: ArrayBuffer, structName?: string) {
+  // get struct
+  const struct = arguments.length === 1 ? this._structs[0] : this.findStruct(structName);
+
+  // get struct fields
+  const fields = struct![2];
+
+  /* local variable */
+  let pos = 0;
+  let offset = 0;
+  let typeSize = 0;
+  let decodedValue: any;
+  const isLittleEndian = this._decodeLittleEndian;
+
+  /* Loop */
+  const tIdx = fields.length;
+  for (let idx = 0; idx < tIdx; idx += 1) {
+    const field = fields[idx];
+    const _typeCode = field[0];
+    const _attr = field[1];
+    const _lenOrIdx = field[2];
+    const _isArr: boolean = (_attr & 0x2) !== 0;
+    const _isVar: boolean = (_attr & 0x4) !== 0;
+
+    // Get array length
+    const _arrayLength = _isVar ? this._decodeFieldDataset[_lenOrIdx] : _isArr ? _lenOrIdx : 1;
+
+    typeSize = CODE_TO_BYTE_SIZE[_typeCode];
+    offset = pos + typeSize * _arrayLength;
+
+    const dv: DataView = new DataView(buffer);
+    const gm: string = `get${CODE_TO_DV_TYPE[_typeCode]}`;
+
+    switch (_attr) {
+      case 0x0:
+        // @ts-ignore
+        decodedValue = dv[gm](pos, isLittleEndian);
+        break;
+      case 0x2:
+      case 0x6:
+        decodedValue = [...Array(_arrayLength).keys()].map((v, i) =>
+          // @ts-ignore
+          dv[gm](i * typeSize + pos, isLittleEndian)
+        );
+        break;
+      case 0x3:
+      case 0x7:
+        decodedValue = new TextDecoder().decode(buffer.slice(pos, offset));
+        break;
+    }
+
+    pos = offset;
+    this._decodeFieldDataset.push(decodedValue);
+    // this._decodeFieldDataset[idx] = decodedValue;
+  }
+
+  return this;
+}
--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@
  */
 import type { StructBlocks } from "./compile";
 import { compile, parseStructAttribute } from "./compile";
+import { decode } from "./decode";
 import { convertToBuffer } from "./encode";
 import { CODE_TO_BYTE_SIZE, CODE_TO_DV_TYPE } from "./types_map";
 
@@ -17,13 +18,13 @@ export class QStruct {
   /*             Properties             */
   /* ---------------------------------- */
 
-  private _rawString: string = ""; // struct descripted string
-  private _fieldNames: string[] = []; // array to store key name
-  private _decodeFieldDataset: any[] = []; // decoded binary data
-  private _structs: StructBlocks = []; // array to store binary parsed data
-  private _littleEndian: boolean = true;
-  private _decodeLittleEndian: boolean = true;
-  private _autoFlush: boolean = false;
+  protected _rawString: string = ""; // struct descripted string
+  protected _fieldNames: string[] = []; // array to store key name
+  protected _decodeFieldDataset: any[] = []; // decoded binary data
+  protected _structs: StructBlocks = []; // array to store binary parsed data
+  protected _littleEndian: boolean = true;
+  protected _decodeLittleEndian: boolean = true;
+  protected _autoFlush: boolean = false;
 
   /* ---------------------------------- */
   /*             Constructor            */
@@ -115,64 +116,7 @@ export class QStruct {
    * @param structName struct name
    * @returns class instance self
    */
-  decode(buffer: ArrayBuffer, structName?: string) {
-    // get struct
-    const struct = arguments.length === 1 ? this._structs[0] : this.findStruct(structName);
-
-    // get struct fields
-    const fields = struct![2];
-
-    /* local variable */
-    let pos = 0;
-    let offset = 0;
-    let typeSize = 0;
-    let decodedValue: any;
-    const isLittleEndian = this._decodeLittleEndian;
-
-    /* Loop */
-    const tIdx = fields.length;
-    for (let idx = 0; idx < tIdx; idx += 1) {
-      const field = fields[idx];
-      const _typeCode = field[0];
-      const _attr = field[1];
-      const _lenOrIdx = field[2];
-      const _isArr: boolean = (_attr & 0x2) !== 0;
-      const _isVar: boolean = (_attr & 0x4) !== 0;
-
-      // Get array length
-      const _arrayLength = _isVar ? this._decodeFieldDataset[_lenOrIdx] : _isArr ? _lenOrIdx : 1;
-
-      typeSize = CODE_TO_BYTE_SIZE[_typeCode];
-      offset = pos + typeSize * _arrayLength;
-
-      const dv: DataView = new DataView(buffer);
-      const gm: string = `get${CODE_TO_DV_TYPE[_typeCode]}`;
-
-      switch (_attr) {
-        case 0x0:
-          // @ts-ignore
-          decodedValue = dv[gm](pos, isLittleEndian);
-          break;
-        case 0x2:
-        case 0x6:
-          decodedValue = [...Array(_arrayLength).keys()].map((v, i) =>
-            // @ts-ignore
-            dv[gm](i * typeSize + pos, isLittleEndian)
-          );
-          break;
-        case 0x3:
-        case 0x7:
-          decodedValue = new TextDecoder().decode(buffer.slice(pos, offset));
-          break;
-      }
-
-      pos = offset;
-      this._decodeFieldDataset.push(decodedValue);
-      // this._decodeFieldDataset[idx] = decodedValue;
-    }
-
-    return this;
-  }
+  decode!: (buffer: ArrayBuffer, structName?: string) => any;
 
   /**
    * Encode object to binary
@@ -297,6 +241,10 @@ export class QStruct {
     this._fieldNames = this.findStruct()![1];
   };
 }
+
+Object.defineProperties(QStruct.prototype, {
+  decode: { value: decode },
+});
 
 /**
  *
